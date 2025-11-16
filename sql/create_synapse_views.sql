d1192ec2c590b455f6f56caa1152859d8c099657
Ashish Rao Gadhe <ashishraogadhe2002@gmail.com>
2025-11-16 23:55:48 +0000

    Create create_synapse_views.sql

---
+++ sql/create_synapse_views.sql
@@ -0,0 +1,14 @@
+-- Serverless view over gold layer in Data Lake
+CREATE OR ALTER VIEW dbo.vw_car_sales_monthly_brand
+AS
+SELECT
+    year_month,
+    brand,
+    country,
+    total_units,
+    total_revenue
+FROM
+    OPENROWSET(
+        BULK 'https://yourstorageaccount.dfs.core.windows.net/gold/car_sales/',
+        FORMAT = 'DELTA'
+    ) AS [g];
